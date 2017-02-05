128bc5798d60f12040a9e80a4a643082edd9e4ab
dscelza <dscelza@umich.edu>
2017-02-05 15:31:34 -0500

    Commented popup.html

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+var dialog;
+
+
+function showDialog(){
+
+    var elem = document.getElementById("record_button");
+    if (elem.value=="Start Recording") elem.value = "Stop Recording";
+    else elem.value = "Start Recording";
+}
+function init() {
+    clicker = document.querySelector('#record_button');
+
+    clicker.addEventListener('click', showDialog, false);
+}
+document.addEventListener('DOMContentLoaded', init);
