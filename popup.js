3b9e0163b5751e200ca471e415eca63cd5a98bbb
dscelza <dscelza@umich.edu>
2017-02-05 15:22:21 -0500

    Bootstrapped html

--- popup.js
+++ popup.js
@@ -6,10 +6,10 @@ function showDialog(){
     var elem = document.getElementById("record_button");
     if (elem.value=="Start Recording") elem.value = "Stop Recording";
     else elem.value = "Start Recording";
-}    
+}
 function init() {
     clicker = document.querySelector('#record_button');
-    
+
     clicker.addEventListener('click', showDialog, false);
-}    
+}
 document.addEventListener('DOMContentLoaded', init);
