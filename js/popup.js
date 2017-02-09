c14c55d683df32c48308d3740f0fc599d3810d46
dscelza <dscelza@umich.edu>
2017-02-09 13:21:04 -0500

    Blue navbar css fix; upload btn click; more recent docs

--- js/popup.js
+++ js/popup.js
@@ -2,14 +2,21 @@ var dialog;
 
 
 function showDialog(){
-
     var elem = document.getElementById("record_button");
     if (elem.value=="Start Recording") elem.value = "Stop Recording";
     else elem.value = "Start Recording";
 }
+
+//Navbar upload btn click
+function upload_btn_click(){
+    
+}
+
 function init() {
     clicker = document.querySelector('#record_button');
-
     clicker.addEventListener('click', showDialog, false);
+
+    upload = document.querySelector('#upload_button');
+    upload.addEventListener('click', upload_btn_click, false);
 }
 document.addEventListener('DOMContentLoaded', init);
