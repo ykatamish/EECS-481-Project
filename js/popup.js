793d1e806580395e86078116daa0d1fba62de74b
dscelza <dscelza@umich.edu>
2017-02-11 18:57:50 -0500

    input btn file picker

--- js/popup.js
+++ js/popup.js
@@ -9,7 +9,15 @@ function showDialog(){
 
 //Navbar upload btn click
 function upload_btn_click(){
+    // document.querySelector('#upload_button').click();
     
+}
+
+//Upload input click. Btn is hidden
+function upload_input_click(){
+    document.getElementById("fileUpload_input").style.left = "1200px";
+    console.log("clicked");
+    console.log(this.value)
 }
 
 function init() {
@@ -18,5 +26,8 @@ function init() {
 
     upload = document.querySelector('#upload_button');
     upload.addEventListener('click', upload_btn_click, false);
+
+    fileupload_input = document.querySelector('#fileUpload_input');
+    fileupload_input.addEventListener('click', upload_input_click, false);
 }
 document.addEventListener('DOMContentLoaded', init);
